bfe1967c5b5c0fa8647768fdbfeabb5a4179ea29
Tony Bowden <tony@tmtm.com>
2018-08-13 13:14:19 +0100

    Find text in header

--- scraper.rb
+++ scraper.rb
@@ -3,10 +3,10 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/User:Oravrattas/Nigerian_House_of_Representatives',
-  xpath: '//table[.//th[.="name"]]//tr[td]/td[1]//a[not(@class="new")]/@title',
-) 
+  xpath: '//table[.//th[contains(.,"name")]]//tr[td]/td[1]//a[not(@class="new")]/@title',
+)
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
 
